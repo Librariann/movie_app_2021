7fc18f4c81589d66b49ee767e4f80d5e4dc04451
SeongHyun Park <okpc0306@naver.com>
2021-05-16 20:54:03 +0900

    react_movie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 //class component
 class App extends React.Component {
@@ -15,6 +16,7 @@ class App extends React.Component {
         data: { movies },
       },
     } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+
     this.setState({ movies, isLoading: false });
     console.log(movies);
   };
@@ -26,10 +28,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">"Loading..."</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -37,9 +43,12 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
